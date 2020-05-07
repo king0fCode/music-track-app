6740b34c25f1b0d3c225e7ffcc71d5cd5eebfeec
unknown <mr.robot.code.94@gmail.com>
2020-05-07 21:01:02 +0530

    music api

--- js/musicApi.js
+++ js/musicApi.js
@@ -1,30 +1,3 @@
-const resultData = function () {
-  let htmldiv = `<div class="rt-tr-group" role="rowgroup">
-    <div class="rt-tr -even" role="row">
-        <div class="rt-td" role="gridcell" style="flex: 100 0 auto; width: 100px;">
-            <div class="Data-reveal" style="animation-fill-mode: both; animation-duration: 1000ms; animation-delay: 0ms; animation-iteration-count: 1; opacity: 1; animation-name: Data-reveal-491590191380387-2;">
-                <img src="https://cdns-images.dzcdn.net/images/cover/be682506145061814eddee648edb7c59/250x250-000000-80-0-0.jpg" class="" style="width: 100px; height: 100px;">
-            </div>
-        </div>
-        <div class="rt-td" role="gridcell" style="flex: 100 0 auto; width: 100px;">
-            <div class="Data-reveal" style="animation-fill-mode: both; animation-duration: 1000ms; animation-delay: 0ms; animation-iteration-count: 1; opacity: 1; animation-name: Data-reveal-491590191380387-3;">
-                Not Afraid
-            </div>
-        </div>
-        <div class="rt-td" role="gridcell" style="flex: 100 0 auto; width: 100px;">
-            <div class="Data-reveal" style="animation-fill-mode: both; animation-duration: 1000ms; animation-delay: 0ms; animation-iteration-count: 1; opacity: 1; animation-name: Data-reveal-491590191380387-3;">
-                Eminem
-            </div>
-        </div>
-        <div class="rt-td" role="gridcell" style="flex: 100 0 auto; width: 100px;">
-            <div class="Data-reveal" style="animation-fill-mode: both; animation-duration: 1000ms; animation-delay: 0ms; animation-iteration-count: 1; opacity: 1; animation-name: Data-reveal-491590191380387-3;">
-                248 seconds
-            </div>
-        </div>
-    </div>
-</div>`
-}
-
 const fetchMusic = async function () {
   const url = 'https://api.deezer.com/search/?q='
   const columnKey = 'TRACK'
